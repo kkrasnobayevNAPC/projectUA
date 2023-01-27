15fae76513b38765b69ef6a6863c28bb42e1c931
Kirill Krasnobayev <kkrasnobayev@napc.com>
2023-01-27 19:35:22 +0200

    initial commit

---
+++ views/site/tenders.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Tenders';
+
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Tenders page
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
